24ac59e87798a3c145b1627ecbf55f74c7d97ce5
Danny <maherdanny70@gmail.com>
2023-05-15 22:22:25 +0100

    Created categories controller, dbContext, add category to db

---
+++ BookShop/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using BookShop.Data;
+using BookShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
