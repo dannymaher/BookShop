c62dfd8f8f478df6c2c121e0fc5c5e2fd7921957
Danny <maherdanny70@gmail.com>
2023-07-11 05:10:27 +0100

    added viewmodel for product, combined edit and create view, added better text editor

--- BookShop.Models/Product.cs
+++ BookShop.Models/Product.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +16,7 @@ namespace BookShop.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        [DisplayName("Category Name")]
+        [DisplayName("Product Name")]
         public string Title { get; set; }
         public string Description { get; set; }
         [Required]
@@ -39,5 +42,13 @@ namespace BookShop.Models
         [Required]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+        
+        public int CategoryId { get;set; }
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
+        public Category Category { get; set; }
+        [ValidateNever]
+        public string ImageUrl { get; set; }
+
     }
 }
--- BookShop/Areas/Admin/Controllers/ProductController.cs
+++ BookShop/Areas/Admin/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
 ﻿using BookShop.DataAccess.Repository;
 using BookShop.DataAccess.Repository.IRepository;
 using BookShop.Models;
+using BookShop.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BookShop.Areas.Admin.Controllers
 {
@@ -18,50 +20,61 @@ namespace BookShop.Areas.Admin.Controllers
         public IActionResult Index()
         {
             List<Product> products = _unitOfWork.Product.GetAll().ToList();
+            
             return View(products);
         }
+        //TODO - Create viewModel for index and remove some attributes from view
+        public IActionResult Upsert(int? id)
+        {
+           
+            //ViewBag.CategoryList = categoryList;
+            //ViewData["CategoryList"] = categoryList;
+            ProductVM ViewModel = new()
+            {
+                CategoryList = _unitOfWork.Category.GetAll().ToList().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                }),
+                Product = new Product()
+            };
+            if(id == null || id == 0)
+            {
+                //create functionality
+                return View(ViewModel);
+            }
+            else
+            {
+                //edit/update functionality
+                ViewModel.Product = _unitOfWork.Product.Get(q => q.Id == id);
+                return View(ViewModel);
+            }
+        }
+        [HttpPost]
+        public IActionResult upsert(ProductVM ProductVM,IFormFile? file)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(ProductVM.Product);
+                _unitOfWork.Save();
+                TempData["success"] = ProductVM.Product.Title + " product created successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
 
-        public IActionResult Create()
-        {
+
+                ProductVM.CategoryList = _unitOfWork.Category.GetAll().ToList().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                });
+                
+                return View(ProductVM);
+            }
             
-            
-            return View();
         }
-        [HttpPost]
-        public IActionResult Create(Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Add(product);
-                _unitOfWork.Save();
-                return RedirectToAction("Index");
-            }
-            return View();
-        }
-        public IActionResult Edit(int? Id)
-        {
-            if(Id == null || Id == 0)
-            {
-                return NotFound();
-            }
-            Product? productFromDB = _unitOfWork.Product.Get(q => q.Id == Id );
-            if(productFromDB == null )
-            {
-                return NotFound();
-            }
-            return View(productFromDB);
-        }
-        [HttpPost]
-        public IActionResult Edit(Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(product);
-                _unitOfWork.Save();
-                return RedirectToAction("Index");
-            }
-            return View();
-        }
+       
         public IActionResult Delete(int? Id)
         {
             if (Id == null || Id == 0)
@@ -86,6 +99,7 @@ namespace BookShop.Areas.Admin.Controllers
             }
             _unitOfWork.Product.Remove(product);
             _unitOfWork.Save();
+            TempData["success"] = product.Title + " product deleted successfully";
             return RedirectToAction("Index");
         }
     }
